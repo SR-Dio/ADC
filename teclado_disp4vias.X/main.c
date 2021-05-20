0568f63da78146da6c42d087bfa71e9c50484ce7
21193665 <21193665@DSN1271158607.cfp127aluno.local>
2021-05-20 14:41:40 -0300

    senha teclado

--- teclado_disp4vias.X/main.c
+++ teclado_disp4vias.X/main.c
@@ -18,7 +18,7 @@ void main(void)
     dispLCD_init();
     
     char cont_senha = 0;
-    char senhaOK = 0;
+    char senhaOK = 1;
     char estado_senha = 0;
     char senha[4] = {0,0,0,0};
     char senha_correta[4] = {'2','1','1','8'};
@@ -53,13 +53,12 @@ void main(void)
                     estado = 10;
                     break;
             case 30:
-                    dispLCD(0,0,"configurando....");
-                    dispLCD(1,0,"                ");
-                    if( tecla == '*')
+                    dispLCD(0,0,"SENHA:          ");
+                    dispLCD(1,0,"                "); 
                     estado = 32;
                     break;  
             case 32:
-                     dispLCD(0,7,"senha");
+                     dispLCD(0,7,senha);
                     if(estado_senha == 100)
                     estado = 39;
                     if(estado_senha ==  90)
@@ -84,16 +83,53 @@ void main(void)
                    estado_senha = 10;
                break;
            case 10:
-               cont_senha = 0;
-               senha[0] = 0;
-               senha[1] = 0;
-               senha[2] = 0;
-               senha[3] = 0;
-               senhaOK = 1;
-               estado_senha = 12;
-               break;
+                    cont_senha = 0;
+                    senha[0] = 0;
+                    senha[1] = 0;
+                    senha[2] = 0;
+                    senha[3] = 0;
+                    senhaOK = 1;
+                    estado_senha = 12;
+                    break;
            case 12:
-               
+                    if(tecla >= '0' && tecla <= '9')
+                        estado_senha = 14;
+                    break;
+           case 14:
+                    senha[cont_senha] = tecla;
+                    estado_senha = 15;
+                    break;
+           case 15:
+                    if(!tecla)
+                       estado_senha = 16;
+                    break;
+           case 16:
+               ++cont_senha;
+               if(cont_senha < 4)
+                   estado_senha = 12;
+               else
+                   estado_senha = 20;
+               break;
+           case 20:
+               senhaOK = 1;
+               for (cont_senha = 0; cont_senha<4; cont_senha++)
+               {
+                   if(senhaOK)
+                       senhaOK = senha[ cont_senha ] == senha_correta[cont_senha ];
+               }
+               if (senhaOK)
+                   estado_senha = 100;
+               else
+                   estado_senha = 90;
+               break;
+           case 90:
+               if (estado == 38)
+                   estado_senha =0;
+               break;
+           case 100:
+               if (estado == 39)
+                   estado_senha = 0;
+               break;
                
        }
     }
